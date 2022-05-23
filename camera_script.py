76147d0a4db626b876eda74d14fc07800ddb0a12
Lollo <lorenz.baraldi@gmail.com>
2022-05-23 13:05:37 +0200

    Added test on frame

--- camera_script.py
+++ camera_script.py
@@ -8,13 +8,19 @@ def camera_script(yooloModel):
 
     while (cap.isOpened()):
         ret, frame = cap.read()
-        if(i == 10):
+        print("prova")
+        if(i == 30):
             results = yooloModel(frame)
-            #TODO: gestire se non c'è una persona
             df = results.pandas()
             person = df.xyxy[0][df.xyxy[0]['class'] == 0]
-            cv2.rectangle(frame, (person['xmin'], person['ymax']), (person['xmax'], person['ymin']), (0, 255, 0), 3)
-            i=0
+            if not person.empty:
+                print(person['xmin'])
+                cv2.rectangle(frame, (int(person['xmin']), int(person['ymax'])), (int(person['xmax']), int(person['ymin'])), (0, 255, 0), 3)
+                #grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                cv2.imshow('Frame', frame)
+                cv2.waitKey(1000)
+                #cv2.imwrite('Images/' + 'Frame' + str(i) + '.jpg', frame)
+                i = 0
         # This condition prevents from infinite looping
         # incase video ends.
         if ret == False:
@@ -22,12 +28,13 @@ def camera_script(yooloModel):
 
         # Save Frame by Frame into disk using imwrite method
         #cv2.imwrite('Images/'+'Frame' + str(i) + '.jpg', frame)
-        cv2.imshow('Frame', frame)
+        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
         i += 1
 
     cap.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    model = torch.hub.load('ultralytics/yolov3', 'yolov3')
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
     camera_script(model)
--- yolo_test.py
+++ yolo_test.py
@@ -1,7 +1,7 @@
 import torch
 
 # Model
-model = torch.hub.load('ultralytics/yolov3', 'yolov3')  # or yolov3-spp, yolov3-tiny, custom
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov3-spp, yolov3-tiny, custom
 
 # Images
 img = 'Data/base.jpeg'  # or file, Path, PIL, OpenCV, numpy, list
