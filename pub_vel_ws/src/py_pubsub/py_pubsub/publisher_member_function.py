34cba30780a73145398fc8498a6eff8513668247
fabrizio <fabri.garuti@gmail.com>
2022-06-01 15:22:20 +0200

    pub vel ws

--- pub_vel_ws/src/py_pubsub/py_pubsub/publisher_member_function.py
+++ pub_vel_ws/src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -17,13 +17,12 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 
-from geometry_msgs.msg import Twist
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.publisher_ = self.create_publisher(String, 'cmd_vel', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
