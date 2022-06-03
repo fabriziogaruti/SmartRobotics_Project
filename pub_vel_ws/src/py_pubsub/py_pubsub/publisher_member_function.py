d599d7f5ece6435b763c862988a4b0340e0f5033
Lollo <lorenz.baraldi@gmail.com>
2022-06-03 12:09:11 +0200

    Added case stop and straight on

--- pub_vel_ws/src/py_pubsub/py_pubsub/publisher_member_function.py
+++ pub_vel_ws/src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -23,9 +23,13 @@ class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
-        timer_period = 0.5  # seconds
+        timer_period = 1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
+        self.dict_vel = {
+            0:{'linear_x': 0},
+            1:{'linear_x':0.5}
+        }
 
     def timer_callback(self):
         #msg = String()
@@ -33,7 +37,6 @@ class MinimalPublisher(Node):
         #self.publisher_.publish(msg)
         #self.get_logger().info('Publishing: "%s"' % msg.data)
         #self.i += 1
-        cmd_vel_array = [0.01,0.02,0.03,0.04,0.05]
         vel = Twist()
         '''for i in range(len(cmd_vel_array)):
             vel.linear.x = cmd_vel_array[i]
@@ -44,8 +47,8 @@ class MinimalPublisher(Node):
             value = f.read()  # perform file operations
             print("Read file", value)
 
-        vel.linear.x = cmd_vel_array[3]
-        print("Publishing velocity" + str(cmd_vel_array[3]))
+        vel.linear.x = self.dict_vel[int(value)]['linear_x']
+        print("Publishing velocity" + str(vel.linear.x))
         self.publisher_.publish(vel)
 
 
