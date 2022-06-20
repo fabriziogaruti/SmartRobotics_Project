8a592a19eb0e34dde6219fb047f2559d239263f3
lorenzbaraldi <lorenz.baraldi@gmail.com>
2022-06-20 15:22:11 +0200

    Fixed inverse kinematics

--- robot/invers_k.py
+++ robot/invers_k.py
@@ -4,12 +4,13 @@ from ikpy.chain import Chain
 from ikpy.link import OriginLink, URDFLink
 import matplotlib.pyplot
 from mpl_toolkits.mplot3d import Axes3D
+import math
 
 arm_chain = Chain(name='left_arm', links=[
     OriginLink(),
     URDFLink(
       name="arm_base_joint",
-      origin_translation=[0, 0, 15],
+      origin_translation=[0, 0, 0.15],
       origin_orientation=[0.0, 0.0, 0.0],
       rotation=[0, 0, 1],
     ),
@@ -18,12 +19,14 @@ arm_chain = Chain(name='left_arm', links=[
       origin_translation=[-0.05166, 0.0, 0.20271],
       origin_orientation=[0, 0, 1.5708],
       rotation=[0, 1, 0],
+      bounds=(-math.pi/2,math.pi/2)
     ),
     URDFLink(
       name="bottom_wrist_joint",
       origin_translation=[0.0, -0.05194, 0.269],
       origin_orientation=[0, 0, 0],
       rotation=[0, 1, 0],
+      bounds=(-math.pi/2,math.pi/2)
     ),
     URDFLink(
       name="elbow_joint",
@@ -39,7 +42,7 @@ arm_chain = Chain(name='left_arm', links=[
     )
     
 ], active_links_mask=[False,True,True,True,True,True])
-target_position = [ 0.7, 0, 0.25]
+target_position = [ 0.5, -0.4, 0.20]
 ax = matplotlib.pyplot.figure().add_subplot(111, projection='3d')
 
 arm_chain.plot(arm_chain.inverse_kinematics([2, 2, 2]), ax)
